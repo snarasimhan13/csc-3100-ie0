05f6044f872b7c9d59727a3776f15997f4175391
Sanjana N <sanjana.narasimhan.1@gmail.com>
2026-08-24 11:14:29 -0700

    commit with App.jsx modification

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Get things started</h1> {/* Simple modification */}
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
