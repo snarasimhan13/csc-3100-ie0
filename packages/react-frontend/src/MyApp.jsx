dcc1c8da93721dc1a604ee7772d4c4c1a5f8ddc2
Sanjana N <sanjana.narasimhan.1@gmail.com>
2026-09-02 10:10:50 -0700

    ie1 - step 6 commit

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,68 @@
+// src/MyApp.jsx
+
+import React, { useState } from "react";
+import Table from "./Table";
+
+// src/MyApp.jsx
+const characters = [
+  {
+    name: "Charlie",
+    job: "Janitor",
+  },
+  {
+    name: "Mac",
+    job: "Bouncer",
+  },
+  {
+    name: "Dee",
+    job: "Aspring actress",
+  },
+  {
+    name: "Dennis",
+    job: "Bartender",
+  },
+];
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
+  return (
+  <div className="container">
+    <Table characterData={characters} removeCharacter={removeOneCharacter} />
+  </div>
+  );
+
+  /*
+  return (
+    <div className="container">
+      <Table characterData={characters} />
+    </div>
+  );
+  */
+}
+
+export default MyApp;
