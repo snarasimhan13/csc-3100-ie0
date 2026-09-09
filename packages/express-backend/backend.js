7b78f093de8fd00afde65cafd04ff83e7f0c7dce
Sanjana N <sanjana.narasimhan.1@gmail.com>
2026-09-09 12:07:43 -0700

    ie2: step 5 commit

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,6 +38,9 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -58,6 +61,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
